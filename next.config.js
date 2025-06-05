7cd2641c7b66f6815d6101c28711c73afb23af97
Ahmed <KhanAhmedRaza@users.noreply.github.com>
2025-06-05 09:22:47 -0400

    update

--- next.config.js
+++ next.config.js
@@ -3,12 +3,19 @@ const { i18n } = require('./next-i18next.config')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   i18n: {
     locales: ['en', 'ar'],
     defaultLocale: 'en',
+    localeDetection: true,
   },
   images: {
-    domains: ['placeholder.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 }
 
