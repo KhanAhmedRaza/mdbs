58d2cb74debcb82918514c1f4c66600bf2b4e356
Ahmed <KhanAhmedRaza@users.noreply.github.com>
2025-06-10 13:58:42 -0400

    updatdd package json for render

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,13 +13,12 @@ import MainNavigation from './MainNavigation';
 import rtlPlugin from 'stylis-plugin-rtl';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
-import { prefixer } from 'stylis';
 import { useEffect } from 'react';
 
 // Create rtl cache
 const cacheRtl = createCache({
   key: 'muirtl',
-  stylisPlugins: [prefixer, rtlPlugin],
+  stylisPlugins: [rtlPlugin],
 });
 
 // Create ltr cache
